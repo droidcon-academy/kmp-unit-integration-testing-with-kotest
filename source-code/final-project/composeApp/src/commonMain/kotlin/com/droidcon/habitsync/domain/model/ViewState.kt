e039520264428dd3810a6287b9866d1601511b2c
Sahar Asadian <63088252+sahar-as@users.noreply.github.com>
2025-07-26 06:55:42 +0330

    Update

--- source-code/final-project/composeApp/src/commonMain/kotlin/com/droidcon/habitsync/domain/model/ViewState.kt
+++ source-code/final-project/composeApp/src/commonMain/kotlin/com/droidcon/habitsync/domain/model/ViewState.kt
@@ -3,7 +3,7 @@ package com.droidcon.habitsync.domain.model
 import com.droidcon.habitsync.utils.ViewStatus
 
 data class ViewState(
-    val viewSate: ViewStatus = ViewStatus.INITIAL,
+    val viewState: ViewStatus = ViewStatus.INITIAL,
     val habits: List<Habit> = emptyList(),
     val message: String? = null
 )
